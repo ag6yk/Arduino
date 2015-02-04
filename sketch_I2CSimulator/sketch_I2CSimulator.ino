c415eb0aa89833df0c281e8edcbcf8bb275b640a
robertc <robertc@broadcom.com>
2015-02-04 15:13:04 -0800

    Initial coding complete, not yet compiled

--- sketch_I2CSimulator/sketch_I2CSimulator.ino
+++ sketch_I2CSimulator/sketch_I2CSimulator.ino
@@ -83,6 +83,45 @@ volatile int  i2cConfig = 0;
 unsigned char  accelAddress;
 unsigned char  accelReadState;
 unsigned char  accelWriteLength;
+unsigned char  accelDataIndex;
+
+// Fake accelerometer data
+// Model a burst with a turn
+const struct ACCEL_DATA accelSimData[32] =
+{
+    { 0, 0, 0, 0, 0, 0 },
+    { 1, 1, 0, 0, 0, 0 },
+    {10, 10, 0, 0, 0, 0},
+    {20, 20, 0, 0, 0, 0},
+    {30, 30, 0, 0, 0, 0},
+    {40, 40, 0, 0, 0, 0},
+    {50, 50, 0, 0, 0, 0},
+    {0xFF, 45, 1, 1, 0, 0},
+    {0xFF, 85, 2, 2, 0, 0},
+    {0xFF, FE, 10, 10, 0, 0},
+    {0, 0, 20, 20, 0, 0},
+    {0, 0, 30, 30, 0, 0},
+    {0, 0, 10, 10, 0, 0}, 
+    {0, 0, 10, 10, 0, 0}, 
+    {0, 0, 10, 10, 0, 0}, 
+    {0, 0, 10, 10, 0, 0}, 
+    {0, 0, 10, 10, 0, 0}, 
+    {0, 0, 10, 10, 0, 0}, 
+    {0, 0, 10, 10, 0, 0}, 
+    {0, 0, 10, 10, 0, 0}, 
+    {0, 0, 5,  5, 0, 0},
+    {0, 0, 2,  2, 0, 0},
+    {0, 0, 1, 1, 0, 0},
+    {0, 0, 0, 4, 0, 0},
+    {0, 0, 0, 2, 0, 0},
+    {0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0}
+};
 
 // Barometer simulator
 unsigned char  baroAddress;
@@ -137,6 +176,7 @@ void setup(void)
         accelAddress = 0;
         accelReadState = 0;
         accelWriteLength = 0;
+        accelDataIndex = 0;
         Wire.begin(ACCEL_IC_ADDRHI);
         Wire.onReceive(accelReceiveEvent);
         Wire.onRequest(accelRequestEvent);
@@ -217,7 +257,7 @@ void accelReceiveEvent(int howMany)
     {
       // Read data and echo it
       accelData = Wire.read();
-      Serial.print("I2C Write to ");
+      Serial.print("accel: I2C Write to ");
       Serial.print(accelAddress, HEX);
       Serial.print(" Data = ");
       Serial.println(accelData, HEX);
@@ -228,6 +268,7 @@ void accelReceiveEvent(int howMany)
       Serial.println("accelReceiveEvent state error");
       accelReadState = 0;
     }
+  }
 }
 
 void accelRequestEvent(void)
@@ -235,11 +276,44 @@ void accelRequestEvent(void)
   // Process the possible requests
   switch(accelAddress)
   {
+    // Device ID - one byte
     case A_DEVID:
+    {
+        Wire.write(byte(A_I_BETTER_BE));
+        accelReadState = 0;
+        break;
+    }
+    
+    // Acceleration data - six bytes
     case A_DATA_X0:
+    {
+        Wire.write(byte(accelSimData[accelDataIndex].xLSB));
+        Wire.write(byte(accelSimData[accelDataIndex].xMSB));
+        Wire.write(byte(accelSimData[accelDataIndex].yLSB));
+        Wire.write(byte(accelSimData[accelDataIndex].yMSB));
+        Wire.write(byte(accelSimData[accelDataIndex].zLSB));
+        Wire.write(byte(accelSimData[accelDataIndex].zMSB));
+        accelDataIndex++;
+        if(accelDataIndex > 31)
+        {
+            accelDataIndex = 0;
+        }
+        accelReadState = 0;
+        break;
+    }
+     
+    // FIFO status - one byte        
     case A_FIFO_STS:
-    case A_FIFO_CNT:
+    {
+        // No event, 9 words waiting
+        Wire.write(byte(0x09));
+        accelReadState = 0;
+        break;
+    }
+    
     default:
+        // Let the master timeout
+        accelReadState = 0;
   }
 }
     
@@ -247,28 +321,299 @@ void accelRequestEvent(void)
 // Barometer simulator
 void baroReceiveEvent(int HowMany)
 {
+  // barometer implements single byte writes by default
+  unsigned char baroData;
+  
+  while(Wire.available())
+  {
+    // Register address
+    if(baroReadState == 0)
+    {
+      baroAddress = Wire.read();
+      baroReadState++;
+    }
+    else if(baroReadState == 1)
+    {
+      // Read data and echo it
+      baroData = Wire.read();
+      Serial.print("baro: I2C Write to ");
+      Serial.print(baroAddress, HEX);
+      Serial.print(" Data = ");
+      Serial.println(baroData, HEX);
+    }
+    else
+    {
+      Serial.println("baroReceiveEvent state error");
+      baroReadState = 0;
+    }
+  }
 }
 
 void baroRequestEvent(void)
 {
+    
+    switch(baroAddress)
+    {
+        // EEPROM - 16 bits
+        case B_EEPROM:
+        case (B_EEPROM+2):
+        case (B_EEPROM+4):
+        case (B_EEPROM+6):
+        case (B_EEPROM+8):
+        case (B_EEPROM+0x0A):
+        case (B_EEPROM+0x0C):
+        case (B_EEPROM+0x0E):
+        case (B_EEPROM+0x10):
+        case (B_EEPROM+0x12):
+        case (B_EEPROM+0x14):
+        case (B_EEPROM+0x16):
+        {
+            Wire.write(byte(baroAddress));
+            Wire.write(byte((baroAddress - B_EEPROM)));
+            baroReadState = 0;
+            break;
+        }
+        
+        // Output data
+        case B_DATA_MSB:
+        {
+            Wire.write(byte(0xDE));
+            Wire.write(byte(0xAD));
+            baroReadState = 0;
+            break;
+        }
+        
+        // Wait for the master to timeout
+        default:
+            baroReadState = 0;
+    }
 }
 
 // Compass simulator
 void compassReceiveEvent(int HowMany)
 {
+  // Compass implements multi-byte writes by default
+  unsigned char compassData;
+  boolean       compassMulti = false;
+  unsigned char compassMultiCount = 0;
+  
+  while(Wire.available())
+  {
+    // Register address
+    if(compassReadState == 0)
+    {
+      compassAddress = Wire.read();
+      compassReadState++;
+      
+      // Check for single multi-byte instance
+      if(compassAddress == C_CONFIG_A)
+      {
+        compassMultiCount = 3;
+        compassMulti = true;
+      }
+      else
+      {
+        compassMultiCount = 0;
+        compassMulti = false;
+      }
+      
+    }
+    else if(compassReadState == 1)
+    {
+      // Read data and echo it
+      compassData = Wire.read();
+      Serial.print("compass: I2C Write to ");
+      Serial.print(compassAddress, HEX);
+      Serial.print(" Data = ");
+      Serial.println(compassData, HEX);
+      
+      // Handle multibyte write
+      if(compassMulti)
+      {
+        compassMultiCount--;
+        compassAddress++;
+        if(compassMultiCount == 0)
+        {
+            compassReadState = 0;
+            compassMulti = false;
+        }
+      }
+      else
+      {
+        compassReadState = 0;
+      }
+    }
+    else
+    {
+      Serial.println("compassReceiveEvent state error");
+      compassMulti = false;
+      compassMultiCount = 0;
+      compassReadState = 0;
+    }
+  }
 }
 
 void compassRequestEvent(void)
 {
+    // switch on address
+    switch(compassAddress)
+    {
+        case C_X_MSB:
+        {
+            // Output Data - six bytes
+            Wire.write(byte(0x12));
+            Wire.write(byte(0x34));
+            Wire.write(byte(0x56));
+            Wire.write(byte(0x78));
+            Wire.write(byte(0x9A));
+            Wire.write(byte(0xBC));
+            compassReadState = 0;
+            break;
+        }
+
+        // Status
+        case C_STATUS:
+        {
+            // Data available, registers not locked
+            Wire.write(byte(C_STS_RDY));
+            compassReadState = 0;
+            break;
+        }
+            
+        // ID
+        case C_ID_A
+        {
+            // Fixed value
+            Wire.write(byte(C_ID_A_VAL));
+            compassReadState = 0;
+            break;
+        }
+
+        // Let master timeout
+        default:
+            compssReadState = 0;
+    }
 }
 
 // Gyro simulator
 void gyroReceiveEvent(int HowMany)
 {
+  // Gyroscope can support multi-byte writes
+  unsigned char gyroData;
+  boolean       gyroMulti = false;
+  unsigned char gyroMultiCount = 0;
+  
+  while(Wire.available())
+  {
+    // Register address
+    if(gyroReadState == 0)
+    {
+      gyroAddress = Wire.read();
+      gyroReadState++;
+      if(gyroAddress & 0x80)
+      {
+        // Only sequenced set
+        gyroMultiCount = 5;
+        gyroMulti = true;
+      }
+      else
+      {
+        gyroMultiCount = 0;
+        gyroMulti = false;
+      }
+      
+    }
+    else if(gyroReadState == 1)
+    {
+      // Read data and echo it
+      gyroData = Wire.read();
+      Serial.print("gyro: I2C Write to ");
+      Serial.print(gyroAddress, HEX);
+      Serial.print(" Data = ");
+      Serial.println(gyroData, HEX);
+      
+      // Handle multibyte write
+      if(gryoMulti)
+      {
+        gyroMultiCount--;
+        gyroAddress++;
+        if(gyroMultiCount == 0)
+        {
+            gyroReadState = 0;
+            gyroMulti = false;
+        }
+      }
+      else
+      {
+        gyroReadState = 0;
+      }
+    }
+    else
+    {
+      Serial.println("gyroReceiveEvent state error");
+      gyroMulti = false;
+      gyroMultiCount = 0;
+      gyroReadState = 0;
+    }
+  }
 }
 
 void gyroRequestEvent(void)
 {
+    // Switch one the address
+    switch(gyroAddress)
+    {
+        // ID register
+        case G_WHO_AM_I:
+        {
+            Wire.write(byte(G_I_BETTER_BE));
+            gyroReadState = 0;
+            break;
+        }
+        
+        // Temperature output
+        case G_OUT_TEMP:
+        {
+            Wire.write(byte(0xBC));
+            gyroReadState = 0;
+            break;
+        }
+        
+        // Status register
+        case G_STATUS_REG:
+        {
+            // Data available, no errors
+            Wire.write(byte(0x0F));
+            gyroReadState = 0;
+            break;
+        }
+        
+        // Angular velocities data - six bytes
+        case G_OUT_X_L:
+        {
+            Wire.write(byte(0x10));
+            Wire.write(byte(0x20));
+            Wire.write(byte(0x30));
+            Wire.write(byte(0x40));
+            Wire.write(byte(0x50));
+            Wire.write(byte(0x60));
+            gyroReadState = 0;
+            break;
+        }
+
+        // FIFO status register
+        case G_FIFO_S_REG:
+        {
+            // seven words ready, no errors
+            Wiree.write(byte(0x07));
+            gyroReadState = 0;
+            break;
+        }
+
+        // Default - let the master timeout
+        default:
+            gyroReadState = 0;
+    }
 }
 
 // End if LibertySensorIF.ino
