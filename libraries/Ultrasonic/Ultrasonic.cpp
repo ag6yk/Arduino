cb85d19ddd186db9944795c88ef1a28d1ed05355
osianfion <osianfion@verizon.net>
2015-02-13 23:39:54 -0800

    Fixed hardware driver. Added better out-of-range reporting

--- libraries/Ultrasonic/Ultrasonic.cpp
+++ libraries/Ultrasonic/Ultrasonic.cpp
@@ -43,10 +43,13 @@ Ultrasonic::Ultrasonic(int Trigger, int Echo)
 }
 
 // Read the range data from this sensor
-// Dynamic range: 100 to 18000
+// Dynamic range 2" to 120"
+// Dynamic range reading: 296 to 17760
+// Values for inches: T/148
+// Therefore reported range will run from 2 to 120
 // Sensor will return 36000 for out-of-range
-// Function will return 0 to 121 inches
-// and 242 inches for out-of-range
+// Function will return 2 to 120 for valid range
+// and 255 for out-of-range
 unsigned char Ultrasonic::ReadRange()
 {
 	// Define locals
@@ -54,15 +57,29 @@ unsigned char Ultrasonic::ReadRange()
 	unsigned char Range;
 
 	// Pulse the Ping signal
+	digitalWrite(_trigger, LOW);		// Ensure we have a high-going pulse
+	delayMicroseconds(5);
 	digitalWrite(_trigger, HIGH);
 	// Wait 20 usec
 	delayMicroseconds(20);
 	digitalWrite(_trigger, LOW);
 	// Now time the echo pulse high
 	Tprop = pulseIn(_echo, HIGH, 1000);
-	// Convert to inches (ref: www.robot-electronics.co.uk)
-	Tprop = Tprop / 148;
-	// Truncate - dynamic range is 248 to 120 so byte is OK
+	// Check for no detection
+	if(Tprop > 30000)
+	{
+		return(255);
+	}
+
+	// Valid range
+	// Convert to inches (ref: www.robot-electronics.co.uk) - divide by 148
+	// TODO - convert division to Horner polynomial
+	// x/148 ~= (x>>8 + x>>9 + x>>11+  x>>13 + x>>15) error of 0.2%
+	// Temp = (Tprop >> 8) + (Tprop >> 9) + (Tprop >> 11) + (Tprop >> 13) + (Tprop >> 15);
+	// return (unsigned char)Temp;
+	Tprop = Tprop / 2;
+	Tprop = Tprop / 74;
+	// Truncate to byte
 	Range = (unsigned char)Tprop;
 
     // return result
